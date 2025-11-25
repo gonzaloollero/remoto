32e697ca723609097b31b61413a23094ae0489ee
Gonzalo <gonzalo.ollero@educa.madrid.org>
2025-11-25 18:27:44 +0100

    Añadidio detector de popochos perezosos

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("¿Cuántos popochos tienes en casa? ");
+            int n = sc.nextInt();
+
+            if (n == 0) {
+                System.out.println("¡Deberías conseguir un popocho!");
+            } else if (n == 1) {
+                System.out.println("Ese popocho está muy mimado.");
+            } else if (n <= 3) {
+                System.out.println("La familia de popochos es equilibrada.");
+            } else {
+                System.out.println("¡Demasiados popochos! No quedará espacio en la cama.");
+            }
+
+            for (int i = 0; i < n; i++) {
+                System.out.println("Zzz...");
         }
     }
 }
